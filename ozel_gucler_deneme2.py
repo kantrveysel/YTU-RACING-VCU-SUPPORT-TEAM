40fc8b5690ed5ec14516328bc77f1b83085b9797
Mehmet Ali Kesici <80796678+alikesici1@users.noreply.github.com>
2021-03-27 05:17:17 +0300

    Update ozel_gucler_deneme2.py

--- ozel_gucler_deneme2.py
+++ ozel_gucler_deneme2.py
@@ -10,12 +10,16 @@ y=0
 
 engel = my_canvas.create_rectangle(x, y, x + 20, y + 500, fill="black")
 ball = my_canvas.create_oval(10, 10, 50, 50, fill='yellow')
+raket1= my_canvas.create_rectangle(470,170 ,490, 250, fill="blue")
+raket2= my_canvas.create_rectangle(20,170 ,40, 250, fill="blue")
 
-boost=my_canvas.create_text(210,15,text="BOOOOST",fill="black")
+boost=my_canvas.create_text(450,15,text="BOOOOST",fill="black")
 stop=my_canvas.create_text(40,15,text="BARRİER",fill="black")
-invisible=my_canvas.create_text(420,15,text="İNVİSİBLE BALL",fill="black")
+invisible=my_canvas.create_text(450,385,text="İNVİSİBLE BALL",fill="black")
+invisible_raket=my_canvas.create_text(60,385,text="İNVİSİBLE RACKET", fill="black")
 
-xspeed = 20
+
+xspeed = 40
 yspeed = 10
 sayac=0
 WIDTH, HEIGHT = 500, 400
@@ -24,6 +28,7 @@ WIDTH, HEIGHT = 500, 400
 
 while True:
     #DENEME AMAÇLI TOP HAREKETİ
+
     my_canvas.move(ball,xspeed,yspeed)
     position=my_canvas.coords(ball)#sol ,üst ,sağ ,alt
     if position[0]<=0 or position[2]>=500:
@@ -36,7 +41,7 @@ while True:
     # SPECİAL1 ENGELDEN SEKECEK
     if sayac ==2 or sayac==13:
         my_canvas.itemconfig(engel,fill="red")
-        my_canvas.itemconfig(stop,fill="blue")
+        my_canvas.itemconfig(stop,fill="magenta")
         if position[0] <= 250 and position[2] >= 200 and xspeed<=0:
             xspeed = -xspeed
             sayac = sayac + 1
@@ -52,32 +57,29 @@ while True:
     if sayac==4 and xspeed>=0  or sayac==15 and xspeed>=0:
         xspeed=60
         my_canvas.itemconfig(ball,fill="red")
-        my_canvas.itemconfig(boost,fill="blue")
+        my_canvas.itemconfig(boost,fill="magenta")
 
     if sayac==4 and xspeed<=0 or sayac==15 and xspeed<=0:
         xspeed=-60
         my_canvas.itemconfig(ball,fill="red")
-        my_canvas.itemconfig(boost,fill="blue")
+        my_canvas.itemconfig(boost,fill="magenta")
 
     #NORMALE DÖNECEK
     if sayac==6 and position[0]>=250 and xspeed>0 or sayac==17 and position[0]>=250 and xspeed>0 :
-        xspeed=20
+        xspeed=30
         my_canvas.itemconfig(ball,fill="yellow")
         my_canvas.itemconfig(boost,fill="black")
 
     if sayac == 6 and position[2] <= 250 and xspeed < 0 or sayac==17 and position[0]>=250 and xspeed<0 :
-        xspeed = -20
+        xspeed = -30
         my_canvas.itemconfig(ball,fill="yellow")
         my_canvas.itemconfig(boost,fill="black")
 
 
-    #TOPU GÖRÜNMEZ YAP, GOLE VE RAKETTEN SEKMESİNE GÖRE YENİDEN AYARLANACAK
+    #TOPU GÖRÜNMEZ YAP, GOLE VE RAKKETTEN SEKMESİNE GÖRE YENİDEN AYARLANACAK
     if sayac==8 or sayac==19:
         my_canvas.itemconfig(ball,fill="black")
-        my_canvas.itemconfig(invisible,fill="blue")
-
-  
-
+        my_canvas.itemconfig(invisible,fill="magenta")
 
     #TOP NORMALDE DÖNER
     if sayac==9 or sayac==20:
@@ -85,11 +87,33 @@ while True:
         my_canvas.itemconfig(invisible,fill="black")
 
 
-    if sayac==22:
+    #RAKETLERİ GÖRÜNMEZ YAP
+    if sayac==11 and xspeed<=0 :
+        my_canvas.itemconfig(raket2,fill="black")
+        my_canvas.itemconfig(invisible_raket,fill="magenta")
+    if sayac == 11 and xspeed >= 0:
+        my_canvas.itemconfig(raket1,fill="black")
+        my_canvas.itemconfig(invisible_raket,fill="magenta")
+    if sayac==22 and xspeed>=0:
+        my_canvas.itemconfig(raket1,fill="black")
+        my_canvas.itemconfig(invisible_raket,fill="magenta")
+    if  sayac==22 and xspeed<=0:
+        my_canvas.itemconfig(raket2,fill="black")
+        my_canvas.itemconfig(invisible_raket,fill="magenta")
+
+    if sayac==12 or sayac==23:
+        my_canvas.itemconfig(raket1,fill="blue")
+        my_canvas.itemconfig(raket2,fill="blue")
+        my_canvas.itemconfig(invisible_raket,fill="black")
+
+    if sayac==25:
         sayac=0
 
     pencere.update()
     time.sleep(0.1)
+
+
+
 
 
 
